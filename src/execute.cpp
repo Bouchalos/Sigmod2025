c696106c52b11a8772ef91311442a97ca6dfe055
Bouchalos <andreasbouchalakis4@gmail.com>
2026-01-25 14:03:03 +0200

    optimal num of threads

--- src/execute.cpp
+++ src/execute.cpp
@@ -527,7 +527,7 @@ ColumnarTable execute(const Plan& plan, [[maybe_unused]] void* context) {
     for (size_t j = 0; j < num_cols; ++j)
         output_table.columns.emplace_back(get<1>(plan.nodes[plan.root].output_attrs[j]));
 
-    int threads = omp_get_max_threads(); // always use max threads
+    int threads = omp_get_max_threads() / 2; // always use max threads
     constexpr size_t chunk_size = 1024;  // fixed chunk size
 
 #pragma omp parallel for schedule(guided) num_threads(threads)  // parallel per-column guided schedule
